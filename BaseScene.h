160ffb939b8dde0d6d42d05e3f6988e8ea84a5ad
gudtldn <gudtldn1234@gmail.com>
2025-03-04 23:40:54 +0900

    fix: 생성자 추가

--- BaseScene.h
+++ BaseScene.h
@@ -5,13 +5,14 @@
 class BaseScene
 {
 public:
+	BaseScene() = default;
 	virtual ~BaseScene() = default;
 
 	// 이동 & 복사 생성자 제거
-	//BaseScene(const BaseScene&) = default;
-	//BaseScene& operator=(const BaseScene&) = default;
-	//BaseScene(BaseScene&&) = default;
-	//BaseScene& operator=(BaseScene&&) = default;
+	BaseScene(const BaseScene&) = delete;
+	BaseScene& operator=(const BaseScene&) = delete;
+	BaseScene(BaseScene&&) = delete;
+	BaseScene& operator=(BaseScene&&) = delete;
 
 	virtual void LoadScene() = 0;
 	virtual void ExitScene() = 0;
