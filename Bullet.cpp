ef3328a44f54cc08ea007909426425633cffd6e5
gudtldn <gudtldn1234@gmail.com>
2025-03-04 23:36:20 +0900

    fix: 헤더 구현파일 바뀜 수정

--- Bullet.cpp
+++ Bullet.cpp
@@ -1,22 +1,16 @@
 #include "Bullet.h"
 
-class Bullet : public CircleObject {
-public:
-	Bullet(EWorld selectedWorld) : CircleObject(selectedWorld) {
+void Bullet::HandleWallCollision(const FVector3& WallNormal)
+{
+    // 탄환 -> 벽 막힘
+}
 
-	};
-	void HandleWallCollision(const FVector3& WallNormal)
-	{
-		// 탄환 -> 벽 막힘
-	}
-
-	void HandleBallCollision(CircleObject& OtherBall)
-	{
-		// Bullet -> Monster
-		// ObjectManager::Get().Destory(bullet)
-		// Monster.넉백
-		// If Monster Die
-		//		Monster.Destroy();
-		//		Player.AddPoint
-	}
-};
+void Bullet::HandleBallCollision(CircleObject& OtherBall)
+{
+    // Bullet -> Monster
+    // ObjectManager::Get().Destory(bullet)
+    // Monster.넉백
+    // If Monster Die
+    //		Monster.Destroy();
+    //		Player.AddPoint
+}
--- Bullet.h
+++ Bullet.h
@@ -1,3 +1,12 @@
 #pragma once
 
 #include "CircleObject.h"
+
+class Bullet : public CircleObject
+{
+public:
+    Bullet(EWorld selectedWorld) : CircleObject(selectedWorld) {}
+
+    virtual void HandleWallCollision(const FVector3& WallNormal) override;
+    virtual void HandleBallCollision(CircleObject& OtherBall) override;
+};
