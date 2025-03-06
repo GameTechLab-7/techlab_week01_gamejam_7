b66eb7e8f166164c59ee30d988f39c55a76836d2
gudtldn <gudtldn1234@gmail.com>
2025-03-06 11:16:12 +0900

    feat: UIObject에 텍스처 로딩 기능 추가 및 렌더링 방식 개선

--- UIObject/UIObject.cpp
+++ UIObject/UIObject.cpp
@@ -2,18 +2,14 @@
 
 UIObject::UIObject(EScene currentScene) {
     MyScene = currentScene;
+
+    Texture = UTexture2D::LoadTargaFromFile("Assets/Texture/default.tga");
+    Texture->SetPrimitiveType(EObjectType::UI);
 }
 
 void UIObject::Render(const URenderer& Renderer) const {
     Renderer.UpdateConstant(Location , Scale, 0);
-    ID3D11Buffer* buffer = Renderer.GetVertexBuffer(EObjectType::UI);
-    int NumOfVertices = Renderer.GetBufferSize(EObjectType::UI);
-
-    //std::cout  << ( buffer != nullptr )  << "\n";
-    if (buffer != nullptr)
-    {
-        Renderer.RenderPrimitive(buffer , NumOfVertices);
-    }
+    Texture->Render(Renderer);
 }
 
 void UIObject::Update(float DeltaTime) {
--- UIObject/UIObject.h
+++ UIObject/UIObject.h
@@ -2,6 +2,7 @@
 #include <memory>
 #include <functional>
 #include "URenderer.h"
+#include "Texture/UTexture2D.h"
 
 class UIObject : public std::enable_shared_from_this<UIObject>
 {
@@ -21,6 +22,8 @@ public:
 	void OnClick() { if(OnClickEvent != nullptr) OnClickEvent(); }
 	void SetOnClickEvent(void(*Fn)()) { OnClickEvent = Fn; }
 private:
+	std::unique_ptr<UTexture2D> Texture;
+
 	FVector3 Location;
 	FVector3 Scale;
 	EScene MyScene;
